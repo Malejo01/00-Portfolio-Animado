d35abad2f7102a51235f3db63154727ddb342c47
Malejo01 <lizarragamauroalejandro@gmail.com>
2024-09-06 19:36:49 -0300

    no renderizaaa

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,21 @@ const ServiceCard = ({index,title,icon}) => {
     <Tilt className= "xs:w-[250px] w-full" >
       <motion.div
         variants={fadeIn("rigth","spring", 0.5 * index, 0.75)}
-        className='w-full green-pink-gradient p-[1px] rounded - [20px] shadow-card'
-      />
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+        options ={{
+          max:45,
+          scale:1,
+          speed:450
+        }} 
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title}
+            className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text- [20px] font-bold text-center'>{title}</h3>
+        </div>
+    </motion.div>
     </Tilt>
   )
 }
@@ -41,6 +54,7 @@ const About = () => {
 
     <div className='mt-20 flex flex-wrap gap-10 '>
       {services.map((service,index) => {
+        console.log("recibiendo datos");
         <ServiceCard key={service.title} index={index} {...service}/>
       })}
     </div>
