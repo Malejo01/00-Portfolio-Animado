950c0ecf2262eb751c881b2b344daf9e57ace3d4
Malejo01 <lizarragamauroalejandro@gmail.com>
2024-09-08 23:17:17 -0300

    Listos los testimonios

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,6 @@ import {github} from '../assets'
 import {SectionWrapper} from './hoc'
 import {projects} from '../constants'
 import {fadeIn, textVariant} from '../utils/motion'
-import { max } from "three/examples/jsm/nodes/Nodes.js"
 
 
 
